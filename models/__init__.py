29ea011af559bc54ef93081a4eff32dbefff8c1e
MusapYildiz <musapyildiz2134@gmail.com>
2026-05-06 16:01:55 +0300

    fix: models __init__ load_backbone fonksiyonu kaldırıldı

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,10 @@
 from models.backbones import ResNet1D, SEResNet1D, InceptionTime1D
 from models.heads     import LinearHead, MLPHead, KANHead
 from models.registry  import (
-    FeatureExtractor, EmbeddingClassifier, build_model,
-    save_checkpoint, load_checkpoint,
-    load_backbone_into_embedding_classifier, get_param_groups,
+    FeatureExtractor,
+    EmbeddingClassifier,
+    build_model,
+    save_checkpoint,
+    load_checkpoint,
+    get_param_groups,
 )
